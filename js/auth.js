34faa5638debce1bf4cbf93e1c8b5d5b6b4e1b6a
这次第“ <1150390191@qq.com>
2019-07-18 23:00:55 +0800

    Site updated: 2019-07-18 23:00:54

--- js/auth.js
+++ js/auth.js
@@ -3,18 +3,21 @@
     const day = 60 * 60 * 24 * 1000;
     const { is_post, lock, passwords, root } = window.AD_CONFIG;
 
-    if(is_post === false || lock === false) {
+    //判断是否是文章、是否锁上了
+
+    if(is_post === false || lock === true) {
       return;
     }
 
     let [password, expires] = atob(window.localStorage.getItem('auth')).split(':'),
       now = new Date().getTime();
 
-    // if(passwords.includes(password) && now < expires) {
-    //   return; 
-    // }
+    //判断密码是否正确？、判断是否过了有效期(includes() 方法用来判断一个数组是否包含一个指定的值，如果是返回 true，否则false。)
+    if(passwords.includes(password) && now < expires) {
+      return; 
+    }
 
-    password = prompt('输入您的名称小写全拼 (例如: 李三 => lisan)');
+    password = prompt('输入暗号：博客作者的名称小写全拼 (例如: 李三 => lisan)');
     password = sha256(password || '');
 
     if(passwords.includes(password)) {
